ac8786b3e2f42fbb6bbd3b3a5e7b97ebb5ec6a9e
Patais <loanbillonnet@gmail.com>
2022-05-06 20:52:13 +0200

    Add fontawesome

--- base/settings.py
+++ base/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Dependencies
+    'fontawesomefree',
+
+    # Modules
     'nifleur'
 ]
 
