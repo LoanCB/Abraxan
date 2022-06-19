3e6a2ed49d78fb58a1122b07710bedaef10d1366
Patais <loanbillonnet@gmail.com>
2022-06-19 11:45:46 +0200

    WIP create user (doesn't work)

--- nifleur/forms.py
+++ nifleur/forms.py
@@ -6,9 +6,10 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2', 'is_staff')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'is_staff')
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-input'
+        self.fields['is_staff'].widget.attrs['class'] = ''
